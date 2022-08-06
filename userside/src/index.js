e438cf56c7e4fd7af033a23ed253815ee56e97dc
Daisy Watt <daisyalexandrawatt@gmail.com>
2022-08-06 23:36:30 +1000

    set up redux

--- userside/src/index.js
+++ userside/src/index.js
@@ -3,7 +3,11 @@ require('file-loader?name=[name].[ext]!./index.html')
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from "./App";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compse } from "redux";
+import thunk from "redux-thunk";
 
+const store = createStroe(reducers, compose(applyMiddleware(thunk)));
 const root = document.getElementById("react-root");
 
 ReactDOM.render(<App/>, root);
