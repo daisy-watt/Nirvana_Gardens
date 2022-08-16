b6db85f8d42139db67a5bd4c3c0a45f81533ceb5
Daisy Watt <daisyalexandrawatt@gmail.com>
2022-08-16 21:17:57 +1000

    addition of front end form

--- userside/src/components/AboutPage/AboutPage.jsx
+++ userside/src/components/AboutPage/AboutPage.jsx
@@ -4,15 +4,19 @@ import Typography from '@mui/material/Typography';
 import Card from '@mui/material/Card';
 import Stack from '@mui/material/Stack'
 import CardActions from '@mui/material/CardActions';
+import TextField from '@mui/material/TextField';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Image from '../asset/jordan.jpg';
+import Image2 from '../asset/Garden_2.jpg';
 
 
 export default function AboutPage(){
+
+
     return (
     <Container color="#01652F">
         <Card >
@@ -44,6 +48,43 @@ export default function AboutPage(){
                     height="400"
                     src={Image}
                 />
+            </Stack>
+        </Card>
+        <Card>
+            <Stack direction="row">
+                <Paper />
+                <CardMedia
+                    component="img"
+                    alt="garden"
+                    height="400"
+                    src={Image2}
+                />
+                    <CardActions >
+                <Box
+
+                    component="form"
+                    sx={{
+                    '& .MuiTextField-root': { m: 1, width: '50vh' },}}
+                    noValidate
+                    autoComplete="off"
+                    >
+                    <Stack alignItems="center">
+                    <Typography gutterBottom variant="h5" color="#01652F" component="div">
+                        contact
+                    </Typography>
+                    <TextField id="standard-basic" label="your name"  />
+
+                    <TextField id="outlined-multiline-flexible" label="ask anything you like" 
+                        rows={6} 
+                        multiline 
+                        variant="outlined"
+                        />
+                    <Button >
+                        get in touch
+                    </Button>
+                    </Stack> 
+                </Box>
+                 </CardActions>
             </Stack>
         </Card>
 
